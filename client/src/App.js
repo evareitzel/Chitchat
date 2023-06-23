8e076bbde4388f1515a0eda43776ebd973a9b1bf
Eva Reitzel <evareitzel@gmail.com>
2023-06-23 10:31:36 -0400

    using hard-coded user obj in App.js, not fetch, to access logged-in app Nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,32 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import Nav from './components/Nav'
-import Login from './pages/Login'
+// import Login from './pages/Login'
 
 function App() {
-  const [user, setUser] = useState(null)
+  // const [user, setUser] = useState(null)
 
-  // DEV testing
+  // [-- DEV testing
+  const [user, setUser] = useState({ "id": 1, "username": "Momo" })
+
   useEffect(() => {
     fetch('/user')
     .then(r => r.json())
     .then(r => console.log(`Users: ${r}`))
   }, [])
+  // end DEV testing --]
 
+  
+  // useEffect(() => {
+  //   // auto-login
+  //   fetch("/me").then(res => {
+  //     if (res.ok) {
+  //       res.json().then(user => setUser(user))
+  //     }
+  //   })
+  // }, [])
 
-  useEffect(() => {
-    // auto-login
-    fetch("/me").then(res => {
-      if (res.ok) {
-        res.json().then(user => setUser(user))
-      }
-    })
-  }, [])
-
-  if (user) {
+  // if (user) {
     return (
       <main className="Main">
         <h1>🗪 chitchat</h1>
@@ -31,9 +34,9 @@ function App() {
         <Nav user={user} onLogout={() => setUser(null)} />
       </main>
     )  
-  } else {
-    return <Login onLogin={user => setUser(user)} />
-  }
+  // } else {
+    // return <Login onLogin={user => setUser(user)} />
+  // }
 }
 
 export default App
--- client/src/features/groups/GroupsContainer.js
+++ client/src/features/groups/GroupsContainer.js
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux"
 import Groups from "./Groups"
 
 function GroupsContainer() {
-  const  groups = useSelector(state => state.groups.entities)
+  const  groups = useSelector(state => state.groups)//.entities)
   return(
     <Groups groups={groups} />
   )
