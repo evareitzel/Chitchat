1b23e55b23a7c0fa84d2af526bb108483bc76ecc
Eva Reitzel <evareitzel@gmail.com>
2023-06-23 11:19:59 -0400

    Lots of unsuccessful fetch testing, mostly in GroupsContainer for READ /groups // RES: https://medium.com/swlh/quick-guide-for-fetching-api-data-using-react-redux-and-hooks-with-explanation-10503726bc6b

--- client/src/features/groups/GroupsContainer.js
+++ client/src/features/groups/GroupsContainer.js
@@ -1,11 +1,45 @@
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
 import Groups from "./Groups"
+import { useEffect } from "react"
 
 function GroupsContainer() {
-  const  groups = useSelector(state => state.groups)//.entities)
+  const groups = useSelector(state => state.groups) // .entities)
+
+  const dispatch = useDispatch() 
+
   return(
-    <Groups groups={groups} />
+    <>
+      <Groups groups={groups} />
+    </>
   )
 }
 
 export default GroupsContainer
+
+
+  // [-- DEV testing (not wkg))
+
+      // [JSONViewer] Your json was stored into 'window.json', enjoy!  
+
+  //   useEffect(() => {
+  //     fetch('/groups')
+  //     .then(r => r.json())
+  //     .then(r => console.log(`Groups: ${r}`))
+  //     .then(g => dispatch({type: "SET_GROUPS", groups: g}))
+  //   }, [])
+
+  // console.log(`Groups: ${window.json}`)
+  // console.log(document.querySelector(window.json))
+  // console.log(groups)
+
+  // const showGroups = () => groups.map(g => {
+  //   return (
+  //     <div>
+  //       {g.name}
+  //     </div>
+  //   )
+  // })
+
+  // end DEV testing --]
+
+  // {showGroups} {/* DEV testing */}
---
+++ client/src/features/groups/groupsSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const groupsSlice = 
+createSlice({
+  name: "groups",
+  initialState: {
+    entities: [],
+  },
+  reducers: {},
+});
+
+export default groupsSlice.reducer;
