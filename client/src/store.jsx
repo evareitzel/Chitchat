ee1559db835994ab662fc5b99d4c78f4384e9565
Eva Reitzel <evareitzel@gmail.com>
2023-07-07 00:53:42 -0400

    Refactor store

--- client/src/store.jsx
+++ client/src/store.jsx
@@ -1,24 +1,28 @@
 import { configureStore } from "@reduxjs/toolkit"
-
 import userReducer from './features/userSlice'
 import groupsReducer from './features/groupsSlice'
-import messagesReducer from './features/messagesSlice'
+import messageReducer from './features/messageSlice'
 // import reactionsReducer from './features/reactionsSlice'
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     groups: groupsReducer,
-    messages: messagesReducer,
+    message: messageReducer,
     // reactions: reactionsReducer
   },
-  middleware: (getDefaultMiddleware) => 
-  getDefaultMiddleware({
-    // serializableCheck: {
-    //   // Ignore these action types
-    //   ignoredActions: ['user/logout/fulfilled', 'appointment/delete/fulfilled', 'user/delete/fulfilled'],
-    // },
-  }),
 })
 
 export default store
+
+
+//, applyMiddleware } from "redux"
+// import thunk from "redux-thunk"
+
+  // middleware: (getDefaultMiddleware) => 
+  // getDefaultMiddleware({
+  //   // serializableCheck: {
+  //   //   // Ignore these action types
+  //   //   ignoredActions: ['user/logout/fulfilled', 'appointment/delete/fulfilled', 'user/delete/fulfilled'],
+  //   // },
+  // }),
